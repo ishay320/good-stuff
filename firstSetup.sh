a785dc1a1646294f98a739ef266193c2da026255
Ishay Trattner <65709059+ishay320@users.noreply.github.com>
2023-05-09 00:07:48 +0300

    added removal of the ctrl shift alt up/down

--- firstSetup.sh
+++ firstSetup.sh
@@ -3,7 +3,7 @@
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y \
-    git cmake make gcc g++ clang gdb \
+    git cmake make gcc g++ clang gdb valgrind \
     python3 python-is-python3 \
     wget nodejs
 
@@ -17,8 +17,12 @@ timedatectl set-local-rtc 1 --adjust-system-clock
 
 # install vscode
 sudo snap install --classic code
-echo "Don't forget to go to setting and change the 'ctrl + alt + shift + up/down' to something else and then return them"
+gsettings set org.gnome.shell.keybindings move-to-workspace-up "[]"
+gsettings set org.gnome.shell.keybindings move-to-workspace-down "[]"
+echo "Check that the 'ctrl + alt + shift + up/down' hase been removed"
 
+echo "Download the newest in https://github.com/hluk/CopyQ"
 sudo apt install copyq -y
 gsettings set org.gnome.shell.keybindings toggle-message-tray "['<Super>m']"
 echo "Don't forget to go to copyq setting and add 'super + v' shortcut and autostart"
+echo "You need to fix the autostart :  in ~/.config/autostart/copyq.desktop: Exec=env QT_QPA_PLATFORM=xcb copyq"
