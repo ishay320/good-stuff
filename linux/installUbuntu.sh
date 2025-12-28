f1caf93aa68c09ed4edb9c8a1fb0f633b62e440b
ishay320 <ishay320@hotmail.com>
2025-12-28 14:57:02 +0200

    refactor(install script): removed dotfile stuff from ubuntu

--- linux/installUbuntu.sh
+++ linux/installUbuntu.sh
@@ -3,10 +3,10 @@
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y \
-    git cmake make gcc g++ clang gdb valgrind \
-    python3 python-is-python3 nodejs \
-    wget curl wireshark \
-    gnuplot-qt tmux xclip htop
+	git cmake make gcc g++ clang gdb valgrind \
+	python3 python-is-python3 nodejs \
+	wget curl wireshark \
+	gnuplot-qt tmux xclip htop
 
 # install chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -30,41 +30,3 @@ sudo apt install copyq -y
 gsettings set org.gnome.shell.keybindings toggle-message-tray "['<Super>m']"
 echo "Don't forget to go to copyq setting and add 'super + v' shortcut and autostart"
 echo "You need to fix the autostart :  in ~/.config/autostart/copyq.desktop: Exec=env QT_QPA_PLATFORM=xcb copyq"
-
-# Path to the startup file
-startup_file="$HOME/.bashrc"
-
-## Insert custom less colors for man
-# Check if the colors are already added
-if grep -Fxq "# Custom less colors" "$startup_file"; then
-    echo "Colors already added to $startup_file"
-else
-    cat <<EOF >>"$startup_file"
-# Custom less colors
-default=\$(tput sgr0)
-red=\$(tput setaf 1)
-green=\$(tput setaf 2)
-purple=\$(tput setaf 5)
-orange=\$(tput setaf 9)
-
-export PAGER=less
-export LESS_TERMCAP_mb=\${red}
-export LESS_TERMCAP_md=\${orange}
-export LESS_TERMCAP_me=\${default}
-export LESS_TERMCAP_se=\${default}
-export LESS_TERMCAP_so=\${purple}
-export LESS_TERMCAP_ue=\${default}
-export LESS_TERMCAP_us=\${green}
-EOF
-    echo "Colors added to $startup_file"
-fi
-
-# tmux config
-TMUX_CONF="${HOME}"/.tmux.conf
-cat <<EOF >>"${TMUX_CONF}"
-set -g mouse on
-set -s set-clipboard off
-set -s copy-command 'xclip -i -sel clipboard'
-EOF
-
-tmux source "${TMUX_CONF}"
